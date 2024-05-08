b51c634d83e74df8a1bd2d2c8604058fcb596b1a
matinnasirlahijani <matinnasirlahijani@gmail.com>
2024-05-08 21:10:35 +0330

    News Repository Added

--- app/Services/news/NewsAPI/NewsApiRepository.php
+++ app/Services/news/NewsAPI/NewsApiRepository.php
@@ -3,33 +3,41 @@
 namespace App\Services\news\NewsAPI;
 
 use App\Interface\NewsRepositoryInterface;
+use App\Models\Article;
+use App\Services\news\BaseRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use phpDocumentor\Reflection\Types\Void_;
 
-class NewsApiRepository implements NewsRepositoryInterface
+class NewsApiRepository extends BaseRepository implements NewsRepositoryInterface
 {
 
-    public static function get(Request $request)
+    public static function create(array $data): Void
     {
-        // TODO: Implement get() method.
-//        dd(2);
+        $article = Article::create([
+            'title' => $data['title'] ?? 'N/A',
+            'description' => $data['description'] ?? 'N/A',
+            'author' => $data['author'] ?? 'N/A',
+            'url' => $data['url'] ?? 'N/A',
+            'published_at' => Carbon::parse($data['publishedAt']) ,
+            'source' => 'News Api',
+            'image_url' => $data['urlToImage'] ?? 'N/A',
+            'content' => $data['content'] ?? 'N/A',
+        ]);
+
+        Cache::store('redis')->put("article_$article->id" , $article , config('cache.redis_ttl'));
+
+        Cache::store('redis')->put('articles' , Article::all() , config('cache.redis_ttl'));
     }
 
-    public function getById(string $id)
-    {
-        // TODO: Implement getById() method.
-    }
-
-    public static function create(array $data)
-    {
-        // TODO: Implement create() method.
-    }
-
-    public function update(array $data): bool
+    public function update(array $data): Void
     {
         // TODO: Implement update() method.
     }
 
-    public function destroy(string $id): bool
+    public function destroy(string $id): Void
     {
         // TODO: Implement destroy() method.
     }
