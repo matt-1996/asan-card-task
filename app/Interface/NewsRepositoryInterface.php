a0e3c4c271620de93cfc3553ce57a2b97e0d6143
matinnasirlahijani <matinnasirlahijani@gmail.com>
2024-05-08 21:13:17 +0330

    Code Improved

--- app/Interface/NewsRepositoryInterface.php
+++ app/Interface/NewsRepositoryInterface.php
@@ -3,17 +3,18 @@
 namespace App\Interface;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 interface NewsRepositoryInterface
 {
-    public static function get(Request $request);
+    public static function get(Request $request): Collection;
 
     public function getById(string $id);
 
-    public static function create(array $data);
+    public static function create(array $data): Void;
 
-    public function update(array $data) : bool;
+    public function update(array $data) : Void;
 
-    public function destroy(string $id) : bool;
+    public function destroy(string $id) : Void;
 
 }
